c53659cb661f1e989acf20e98c9f3e147c52194a
JadeXLol <60849283+JadeXLol@users.noreply.github.com>
2020-02-22 18:29:28 -0500

    Add files via upload

---
+++ Time to Decompress.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>    
+
+using namespace std;
+int main() {
+    int num;
+    int int_array[num];
+    char char_array[num]; 
+    int random_int;
+    int controller;
+
+    cin >> num;
+    for (random_int=0; random_int<num; random_int++){
+    cin >> int_array[random_int] >> char_array[random_int];
+    for (controller=0; controller<int_array[random_int];controller++){
+        cout << char_array[random_int];
+    }
+    cout <<endl;
+    }
+    return 0;
+}
